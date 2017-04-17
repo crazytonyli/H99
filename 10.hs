4ca25b3ecf2603f558d494243e8dff3ac96c98dd
Tony Li <i@crazytony.li>
2017-04-17 22:30:33 +1200

    Problem #6

--- 10.hs
+++ 10.hs
@@ -72,3 +72,17 @@ ctl_reverse :: [a] -> [a]
 ctl_reverse [] = []
 ctl_reverse [x] = [x]
 ctl_reverse (x:xs) = ctl_reverse xs ++ [x]
+
+
+-- Problem 6
+-- Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+--
+-- Example:
+-- *Main> isPalindrome [1,2,3]
+-- False
+-- *Main> isPalindrome "madamimadam"
+-- True
+-- *Main> isPalindrome [1,2,4,8,16,8,4,2,1]
+-- True
+ctl_isPalindrome :: Eq a => [a] -> Bool
+ctl_isPalindrome xs = xs == (ctl_reverse xs)
