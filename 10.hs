9dfa78922e3f745cddb42cedf9310a88af0ea907
Tony Li <i@crazytony.li>
2017-04-17 22:17:22 +1200

    Problem #2

--- 10.hs
+++ 10.hs
@@ -1,3 +1,30 @@
+-- Problem 1 to 10
+-- https://wiki.haskell.org/99_questions/1_to_10
+
+
+-- Problem #1
+-- Find the last element of a list.
+--
+-- Example:
+-- Prelude> myLast [1,2,3,4]
+-- 4
+-- Prelude> myLast ['x','y','z']
+-- 'z'
 ctl_last :: [a] -> a
 ctl_last [x] = x
 ctl_last (_:xs) = ctl_last xs
+ctl_last _ = error "Not an empty list"
+
+
+-- Problem #2
+-- Find the last but one element of a list.
+--
+-- Example:
+-- Prelude> myButLast [1,2,3,4]
+-- 3
+-- Prelude> myButLast ['a'..'z']
+-- 'y'
+ctl_but_last :: [a] -> a
+ctl_but_last (x:[_]) = x
+ctl_but_last (x:xs) = ctl_but_last xs
+ctl_but_last _ = error "Not an empty list"
